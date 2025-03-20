3593bccaaf00391be2c1962e767ff51838609f50
Maks_LEAP <msk@leap.dk>
2025-03-20 15:59:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
@@ -45,7 +46,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered', icon="✅")
 
 # New section to display smoothiefroot nutrition information
-import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefruit_response.json(), use_container_width=True)
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
